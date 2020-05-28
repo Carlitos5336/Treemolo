23fe735233ac95af04ab1d7446a8d5c91f00614c
Carlitos5336 <carlosogandomontas@gmail.com>
2020-05-28 11:37:47 -0400

    First commit

---
+++ app/src/main/java/com/example/treemoloapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.treemoloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLimpiar, btnIngresar;
+    EditText correo, contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+        btnIngresar = findViewById(R.id.btnIngresar);
+        correo = findViewById(R.id.editcorreo);
+        contraseña = findViewById(R.id.editcontraseña);
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnLimpiar:
+                limpiar();
+                break;
+            case R.id.btnIngresar:
+                ingresar();
+                break;
+        }
+    }
+
+    private void ingresar() {
+        if(correo.getText().toString() == "Carlosbom" && contraseña.getText().toString() == "123456")
+        {
+            Intent intent = new Intent(this, SuccesActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"EL usuario o la contraseña están incorrectos.",Toast. LENGTH_SHORT);
+        }
+    }
+
+    private void limpiar() {
+        correo.setText("");
+        contraseña.setText("");
+    }
+}
